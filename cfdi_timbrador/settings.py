3265e16eaa091779d7f0ef7d478edf9422d85965
Luis <lquintanillab@gmail.com>
2024-10-17 09:53:33 -0600

    Cfdi impuestos fix2

--- cfdi_timbrador/settings.py
+++ cfdi_timbrador/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_crontab',
     'applications.timbrado_edicom',
     'applications.knzl',
     'applications.api_cfdi',
